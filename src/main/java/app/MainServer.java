03a76c68d1ad7889ae36840dedd376dac50f3c58
Arthur <alves.m.arthur@gmail.com>
2019-10-08 02:15:30 -0300

    Segundo marcador adicionado na view

---
+++ src/main/java/app/MainServer.java
@@ -0,0 +1,30 @@
+package app;
+
+import static spark.Spark.*;
+
+
+public class MainServer {
+	
+	final static Model model = new Model();
+	
+    public static void main(String[] args) {
+
+		// Get port config of heroku on environment variable
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 8080;
+        }
+        port(port);
+
+		//Servir conteudo html, css e javascript
+		staticFileLocation("/static");
+
+		Controller controller = new Controller(model);
+		
+		controller.buscarCarro();
+		
+    }
+}
